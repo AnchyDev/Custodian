44a1af409191144a63d08de821af16cd20090491
AnchyDev <lewienato@gmail.com>
2022-09-15 16:12:54 +1000

    Initial bot setup.

---
+++ Custodian/Bot/BotCustodian.cs
@@ -0,0 +1,26 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custodian.Bot
+{
+    public class BotCustodian
+    {
+        private readonly DiscordSocketClient _client;
+
+        public BotCustodian()
+        {
+            _client = new DiscordSocketClient();
+        }
+
+        public async Task StartAsync()
+        {
+            await _client.LoginAsync(Discord.TokenType.Bot, Environment.GetEnvironmentVariable("CUSTODIAN_TOKEN"));
+            await _client.StartAsync();
+            await Task.Delay(Timeout.Infinite);
+        }
+    }
+}
---
+++ Custodian/Program.cs
@@ -0,0 +1,14 @@
+﻿using Custodian.Bot;
+using System;
+
+namespace Custodian
+{
+    public class Program
+    {
+        public async Task Main(string[] args)
+        {
+            var bot = new BotCustodian();
+            await bot.StartAsync();
+        }
+    }
+}
