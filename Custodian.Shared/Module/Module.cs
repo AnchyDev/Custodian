e2d905ced74ca0cd6d206b747e2e8d5658e91b5b
AnchyDev <lewienato@gmail.com>
2022-10-01 09:03:46 +1000

    Add basic module definition.

---
+++ Custodian.Shared/Module/Module.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Custodian.Shared.Module
+{
+    public abstract class Module
+    {
+        public abstract string Name { get; }
+        public abstract string Description { get; }
+    }
+}
