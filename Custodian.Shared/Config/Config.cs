b79905efc58afe5e41aa44c8750d4e730c48b280
AnchyDev <lewienato@gmail.com>
2022-10-01 09:32:31 +1000

    Implemented basic throwable config loader.

---
+++ Custodian.Shared/Config/Config.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Custodian.Shared.Config
+{
+    public class Config
+    {
+        /// <summary>
+        /// Fetches the deserialized json config from file path.
+        /// </summary>
+        /// <typeparam name="T">The type to deserialize.</typeparam>
+        /// <param name="file">The path to the config.json</param>
+        /// <param name="createIfNotExist">If the config does not exist, create it.</param>
+        /// <returns>The deserialized config.</returns>
+        public async Task<T?> GetAsync<T>(string file, bool createIfNotExist = true) where T : new()
+        {
+            var path = Path.GetDirectoryName(file);
+            var fileName = Path.GetFileName(file);
+            var fullPath = Path.Combine(path, fileName);
+
+            if (!Directory.Exists(path))
+            {
+                if (createIfNotExist)
+                {
+                    Directory.CreateDirectory(path);
+                }
+                else
+                {
+                    throw new DirectoryNotFoundException();
+                }
+            }
+
+            if(!File.Exists(fullPath))
+            {
+                if (createIfNotExist)
+                {
+                    using var cfs = File.Create(fullPath);
+                    await System.Text.Json.JsonSerializer.SerializeAsync<T>(cfs, new T());
+                }
+                else
+                {
+                    throw new FileNotFoundException();
+                }
+            }
+
+            using var rfs = File.Open(fullPath, FileMode.Open, FileAccess.Read);
+
+            return await System.Text.Json.JsonSerializer.DeserializeAsync<T>(rfs);
+        }
+    }
+}
