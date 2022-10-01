7860f8b40a33d6a39b80fc02b3e07814317b6cd1
AnchyDev <lewienato@gmail.com>
2022-10-01 10:19:06 +1000

    Basic module works for overhaul.

---
+++ Custodian.DirectMessage/Modules/ModuleDirectMessage.cs
@@ -0,0 +1,10 @@
+﻿using Custodian.Shared.Modules;
+
+namespace Custodian.DirectMessage.Modules
+{
+    public class ModuleDirectMessage : Module
+    {
+        public override string Name { get => "DirectMessage"; }
+        public override string Description { get => "Allows users to complete actions in a direct message channel with the bot."; }
+    }
+}
